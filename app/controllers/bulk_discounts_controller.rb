63452fbc3f98c006de5707a4012235efb7a7556f
Emily Port <emilylport@gmail.com>
2022-11-11 15:51:34 -0700

    merchant able to create a new discount and new discount is listed on discount show page after creation

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -5,4 +5,19 @@ class BulkDiscountsController < ApplicationController
       @merchant.bulk_discounts
     end
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant.bulk_discounts.create!(bulk_discount_params)
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private 
+  def bulk_discount_params
+    params.permit(:discount, :quantity_threshold)
+  end
 end
