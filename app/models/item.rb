7543a9e80f86d7b678d4cba8b6862d3a5e3adcc6
Emily Port <emilylport@gmail.com>
2022-11-11 14:18:59 -0700

    added many to many relationships to item and bulk discounts spec files

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   belongs_to :merchant 
   has_many :transactions, through: :invoices 
+  has_many :discount_items
+  has_many :bulk_discounts, through: :discount_items 
 
   def most_recent_date
     invoices.order(created_at: :desc).limit(1).pluck(:created_at)
